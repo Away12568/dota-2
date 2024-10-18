5f0f2fd939987016a39f39f8499aa78bee16c4cf
awat <154649552+Away12568@users.noreply.github.com>
2024-10-18 19:03:01 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './components/Card/card'
+import './app.css'
+
+
+
+const App = () => {
+  return (
+   <>
+   
+    <div className="wrap">
+     <Card/>
+     
+    </div>
+
+   </>
+  )
+}
+
+export default App
